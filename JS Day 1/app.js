27bdee762028321a5cc1cdab5bd74237e0d400f9
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-01-27 20:11:51 +0530

    JS Day 1

---
+++ JS Day 1/app.js
@@ -0,0 +1,67 @@
+// let age = 20;
+// var name="Haamid";
+// console.log(name);
+// console.log(age);
+// const nationality="Indian";
+
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// let b=10;
+// let b=100;
+// console.log(b);
+
+// console.log(a);
+// var a=10;
+
+
+// let a="Siddesh"
+// console.log(age)
+// console.log(a);
+
+// Different type of datatype -> String , Number , Boolean
+
+
+// let name=false;
+// console.log(typeof(name))
+
+
+// Conversion of Datatype
+// 1.String - Number to String / Boolean to String
+
+// let num=100;
+// console.log(typeof(num));
+// let a=String(num);
+// console.log(typeof(a));
+
+
+// let bool = true;
+// console.log(typeof(bool));
+// let a=String(bool);
+// console.log(typeof(a));
+
+// 2.Number - String to Number / Boolean to Number
+
+// let name="ritik"
+// console.log(typeof(name));
+// let a=Number(name);
+// console.log(typeof(a));
+
+// let bool=false;
+// console.log(typeof(bool));
+// let a=Number(bool);
+// console.log(typeof(a));
+
+
+// 3.Boolean - String to Boolean / Number to Boolean
+
+// let num=100;
+// console.log(typeof(num));
+// let a=Boolean(num);
+// console.log(typeof(a));
+
+let name='Siddesh';
+console.log(typeof(name));
+let a=Boolean(name);
+console.log(typeof(a));
