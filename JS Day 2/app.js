2ad0bf6bb7d97f81fa225e5e81b33cfa7e27f0d7
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-01-28 20:27:29 +0530

    JS Day 2

---
+++ JS Day 2/app.js
@@ -0,0 +1,100 @@
+// Arithmetic Operator Example +, -, * , / , % , **
+let num1=10;
+// let num2=20;
+
+// let add=num1+num2;
+// console.log(add);  // 30
+
+// let sub=num1-num2;
+// console.log(sub);  // -10
+
+// let mul=num1*num2;
+// console.log(mul); //200
+
+// let div=30/7;
+// console.log(div) //2
+
+// let mod=30%7;
+// console.log(mod) //2
+
+// let sqr=num1**5; 
+// console.log(sqr); //1000
+
+
+// Assignment Operator Example
+// let a=10;
+// console.log(a)
+// a+=20; //a=a+20
+// console.log(a)
+
+// let b=20;
+// console.log(b)
+// b-=10; //a=a-20
+// console.log(b)
+
+// Logical Operator && , || ,!
+// Relational Operator > , < , >= , <= 
+// let age=20;
+// let salary = 1000;
+
+// if(salary != 10000)
+// {
+//     console.log("Person salary is either greater or less than 10000");
+// }
+// else
+// {
+//     console.log("Person salary is 10000");
+// }
+
+// let mark=45;
+
+// if (mark>80)
+// {
+//     console.log("Grade A")
+// }
+
+// else if(mark > 60 && mark<80 )
+// {
+//     console.log("Grade B")
+// }
+
+// else if ( mark > 40 && mark < 60)
+// {
+//     console.log("Grade c")
+// }
+
+// else{
+//     console.log("Grade f")
+// }
+
+
+// loop --> for , while , do while
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+// Initialization / Condition / Increment-Decrement
+
+// let num=11;
+// do
+// {
+//    console.log(num);
+//    num++;
+// }
+// while(num<=10);
+
+// Jump --> Break , Continue
+
+for(let i=1 ; i<=5 ; i++)
+{
+    if(i==2)
+    {
+        continue;
+    }
+    console.log(i);
+}
+console.log('For loop ended!!!')
+
