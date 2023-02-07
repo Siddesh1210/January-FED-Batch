e24282245f80c993cebf21865c7a8e961c9b150d
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-02-07 20:16:46 +0530

    Dataset Commit

--- JS Day 9/app.js
+++ JS Day 9/app.js
@@ -1,13 +1,22 @@
-let button=document.getElementById("btn");
 let Order=document.getElementsByClassName("orderBtn");
+// console.log(Order);
 for(i=0;i<Order.length;i++)
 {
     // console.log(Order[i])
-    Order[i].addEventListener('click',orderFood)
+    Order[i].addEventListener('click',orderFood);
 }
 
 
-function orderFood()
+function orderFood(e)
 {
-    console.log("Order Food");
+    // console.log("Button is clicked!!!");
+    // console.log(e.target.dataset.price)
+    Fname=e.target.dataset.title;
+    price=e.target.dataset.price;
+    let foodName=document.getElementById("title");
+    let foodPrice=document.getElementById("price");
+    foodName.innerHTML=Fname;
+    foodPrice.innerHTML=price;
+    let success=document.getElementById("success-alert");
+    success.classList.remove('d-none')
 }
